ea684ae9e8fb26a36c3d1daa71c43cb880362ce2
Oleg Smitkevich <o.smitkevich@gmail.com>
2024-01-09 20:39:25 +0300

    feat: фикс

---
+++ src/app/main-page/index.tsx
@@ -0,0 +1,5 @@
+import styles from './style.module.scss';
+
+export default function Home() {
+	return <main className={styles['.main']}>Hello world!</main>;
+}
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './style.module.scss'
-
-export default function Home() {
-  return (
-    <main className={styles['.main']}>
-      Hello world!
-    </main>
-  )
-}
