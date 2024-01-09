9589f827be15627882087aeebbe2fb87e7c69fa1
Oleg Smitkevich <o.smitkevich@gmail.com>
2024-01-09 19:26:55 +0300

    Инициализация

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.scss'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Code Wizards',
+  description: 'Мы не просто создаем сайты. Мы строим цифровые пространства, где каждая строчка кода - это мелодия успеха вашего бизнеса',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ru">
+      <body className='body'>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from './style.module.scss'
+
+export default function Home() {
+  return (
+    <main className={styles['.main']}>
+      Hello world!
+    </main>
+  )
+}
