5d4a991d9625bd555cdd055bfc3f2c0247078863
Oleg Smitkevich <o.smitkevich@gmail.com>
2024-01-10 18:54:06 +0300

    fix: fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 // import { Inter } from 'next/font/google'
 import './globals.scss';
 
+// Важно
 // const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
