2f613ab7d090cbcf6995a006ffd48bccaf113a86
Oleg Smitkevich <o.smitkevich@gmail.com>
2024-01-11 17:33:41 +0300

    feat: sdf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import Header from '@/components/Header';
+
 import styles from './style.module.scss';
 
+console.log();
 export default function Home() {
-	return <main className={styles['.main']}>Hello world!</main>;
+	return (
+		<main className={styles['.main']}>
+			<Header />
+		</main>
+	);
 }
