d88b6da158962dd49c179e5efca0f8a3ed6076d5
Oleg Smitkevich <o.smitkevich@gmail.com>
2024-01-12 15:58:40 +0300

    feat: добавляю normalize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
-import Header from '@/components/Header';
-
 import styles from './style.module.scss';
 
 console.log();
 export default function Home() {
-	return (
-		<main className={styles['.main']}>
-			<Header />
-		</main>
-	);
+	return <main className={styles['.main']}></main>;
 }
