7be6361a30abf313c703176b5a0fa4bcc22e771c
JackKerouac1613 <133998734+JackKerouac1613@users.noreply.github.com>
2024-01-14 17:17:19 +0300

    feat: новый компонент

---
+++ src/components/ChoiceCard/index.tsx
@@ -0,0 +1,42 @@
+import IconStar from '@/images/chooseicon';
+
+import { IChoiceItemProps } from './interface';
+import styles from './style.module.scss';
+
+export default function ChooseCard({ item }: IChoiceItemProps) {
+	return (
+		<li
+			className={`${styles['choice-card']} ${
+				styles[`choice-card_${item.modifier}`]
+			}`}
+		>
+			<div
+				className={`${styles['choice-card__wrapper']} ${
+					styles[`choice-card_${item.modifier}__wrapper`]
+				}`}
+			>
+				<div
+					className={`${styles['choice-card__column']} ${
+						styles[`choice-card_${item.modifier}__column`]
+					}`}
+				>
+					{item.iconFill && <IconStar fill={item.iconFill} />}
+					<h4
+						className={`${styles['choice-card__title']} ${
+							styles[`choice-card_${item.modifier}__title`]
+						}`}
+					>
+						{item.title}
+					</h4>
+				</div>
+				<p
+					className={`${styles['choice-card__description']} ${
+						styles[`choice-card_${item.modifier}__description`]
+					}`}
+				>
+					{item.description}
+				</p>
+			</div>
+		</li>
+	);
+}
