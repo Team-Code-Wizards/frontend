7607ea61963f04bcbc3dd91900148bcfe8826417
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2024-01-15 12:24:34 +0300

    feat: создать компонент "О нас", добавить стили для компонента, вставить компонент в app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import AboutUs from '@/components/about-us/page';
+
 import styles from './style.module.scss';
 
 console.log();
 export default function Home() {
-	return <main className={styles['.main']}></main>;
+	return (
+		<main className={styles['main']}>
+			<AboutUs />
+		</main>
+	);
 }
---
+++ src/components/about-us/page.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+
+import laptopImage from '@/images/lap-top-image.png';
+import sparklesImage from '@/images/sparkles.png';
+
+import styles from './style.module.scss';
+
+export default function AboutUs() {
+	return (
+		<div className={styles['about-us']}>
+			<div className={styles['about-us__title']}>
+				<h2>О нас</h2>
+				<h3>
+					Мы – коллектив творческих умов, готовых воплощать <br /> ваши идеи в
+					цифровую реальность.
+				</h3>
+			</div>
+			<div className={styles['about-us__content']}>
+				<div style={{ position: 'relative' }}>
+					<Image src={laptopImage} alt={'Laptop image'} />
+					<Image
+						src={sparklesImage}
+						alt={'Sparkles'}
+						style={{ position: 'absolute', top: '33%', left: '38%' }}
+					/>
+				</div>
+				<div>
+					<div className={styles['about-us__content__goals']}>
+						<p>5 лет опыта</p>
+						<p>Внедрение инноваций</p>
+						<p>Современные технологии</p>
+					</div>
+					<div className={styles['about-us__content__resume']}>
+						<h4>
+							Мы начали как группа энтузиастов, которые верили в силу креатива и
+							технологий. С течением времени наша страсть к веб-разработке
+							превратилась в виртуозное искусство создания сайтов для малого и
+							среднего бизнеса. Мы используем передовые решения для того, чтобы
+							ваш сайт не только соответствовал, но и опережал свое время.
+						</h4>
+						<h4>
+							Пять лет опыта — это не просто цифры, это накопленные знания,
+							глубокое понимание отрасли и надежная платформа для вашего успеха.
+						</h4>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
