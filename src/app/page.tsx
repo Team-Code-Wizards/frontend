e4647cf895da5a3880b091b4abde8f21831e9aaa
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-01-15 17:51:21 +0600

    refactor: добавить переменные стилей в Услуги

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Services from '@/components/Services';
+
 import styles from './style.module.scss';
 
 export default function Home() {
-	return <main className={styles['.main']}></main>;
+	return (
+		<main className={styles['.main']}>
+			<Services />
+		</main>
+	);
 }
