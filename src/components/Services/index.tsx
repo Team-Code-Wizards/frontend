3cc0594b68d44e2df0e508cf6916cc73a65ddd8d
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-01-15 16:04:43 +0600

    feat: добавить блок услуги

---
+++ src/components/Services/index.tsx
@@ -0,0 +1,37 @@
+import { services } from '@/constants';
+
+import styles from './style.module.scss';
+
+export default function Services() {
+	return (
+		<div className={styles['services']}>
+			<h2 className={styles['services__title']}>Услуги</h2>
+			<div className={styles['services__cards']}>
+				{services.map((service) => (
+					<div key={service.title} className={styles['service-card']}>
+						<div className={styles['service-card__content']}>
+							<h4>{service.title}</h4>
+							{service.content.map((text, ind) => (
+								<p key={ind}>{text}</p>
+							))}
+							<span className={styles['service-card__prices']}>
+								<p className={styles['service-card__prices_new']}>
+									от {service.newPrice}$
+								</p>
+								<p className={styles['service-card__prices_old']}>
+									{service.oldPrice}$
+								</p>
+							</span>
+						</div>
+						<span className={styles['service-card__btns']}>
+							<button className={styles['service-card__btns_order']}>
+								Заказать
+							</button>
+							<span className={styles['service-card__btns_ask']}>?</span>
+						</span>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+import services from './Services';
+
+export { services };
