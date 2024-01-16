50c87d216b164558644d49e95ea45fb4918766a2
Egor Krasilnikov <krasilnikov19@gmail.com>
2024-01-16 17:07:57 +0300

    style: верстка компонента Contacts

--- src/components/Contacts/index.tsx
+++ src/components/Contacts/index.tsx
@@ -1,10 +1,124 @@
+'use client';
+
+import { FormEvent } from 'react';
+
+import ArrowIcon from '@/images/icons/ArrowIcon';
+import TelegramBgIcon from '@/images/icons/TelegramBgIcon';
+import ViberIcon from '@/images/icons/ViberIcon';
+import WhatsappIcon from '@/images/icons/WhatsappIcon';
+
 import styles from './style.module.scss';
 
+//TODO добавить корректные ссылки для мессенджеров
+
+//TODO вынести форму в отдельный клиентский компонент
+
+//TODO добавить кнопку очистки инпутов когда они активны
+
 export default function Contacts() {
+	const sendingDataForFeedback = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+	};
+
 	return (
 		<section className={styles['contacts']}>
 			<h1 className={styles['contacts__title']}>Контакты</h1>
-			<div className={styles['contacts__body']}></div>
+			<div className={styles['contacts__body']}>
+				<ul className={styles['link-group']}>
+					<li className={styles['link-box']}>
+						<a className={styles['link']} href="mailto:email@code-wizards.team">
+							<p className={styles['link__item_word-space']}>
+								e-mail email@code-wizards.team
+							</p>
+							<span className={styles['link__item']}>
+								<ArrowIcon />
+							</span>
+						</a>
+					</li>
+					<li className={styles['link-box']}>
+						<a className={styles['link']} href="tel:+7(950)424-13-38">
+							<p className={styles['link__item_tel']}>
+								<span>Телефон</span>
+								<span>+7 (950) 424-13-38</span>
+							</p>
+							<span className={styles['link__item']}>
+								<ArrowIcon />
+							</span>
+						</a>
+					</li>
+					<li className={styles['link-box']}>
+						<a className={styles['link_messenger']} href="http://">
+							<span className={styles['link__item']}>
+								<ViberIcon />
+							</span>
+							<span className={styles['link__item_no-margin']}>viber</span>
+						</a>
+					</li>
+					<li className={styles['link-box']}>
+						<a className={styles['link_messenger']} href="http://">
+							<span className={styles['link__item']}>
+								<TelegramBgIcon />
+							</span>
+							<span className={styles['link__item_no-margin']}>telegram</span>
+						</a>
+					</li>
+					<li className={styles['link-box']}>
+						<a className={styles['link_messenger']} href="http://">
+							<span className={styles['link__item']}>
+								<WhatsappIcon />
+							</span>
+							<span className={styles['link__item_no-margin']}>whatsapp</span>
+						</a>
+					</li>
+				</ul>
+				<div className={styles['form-box']}>
+					<form className={styles['form']} onSubmit={sendingDataForFeedback}>
+						<input
+							className={styles['form__input']}
+							type="text"
+							name="clientName"
+							placeholder="Имя"
+						/>
+						<input
+							className={styles['form__input']}
+							type="tel"
+							name="clientTel"
+							placeholder="Телефон"
+						/>
+						<input
+							className={styles['form__input']}
+							type="email"
+							name="clientEmail"
+							placeholder="E-mail"
+						/>
+						<textarea
+							className={styles['form__input']}
+							name="clientMessage"
+							placeholder="Сообщение"
+						/>
+						<input
+							className={styles['form__input']}
+							type="file"
+							id="file"
+							name="clientFile"
+							accept=".doc"
+						/>
+						<label className={styles['label']} htmlFor="file">
+							Прикрепить ТЗ
+						</label>
+						<span className={styles['form__notice']}>
+							*В формате Документ Word
+						</span>
+						<button className={styles['form__button']} type="submit">
+							Отправить
+						</button>
+						<span className={styles['form__notice']}>
+							Нажимая кнопку «отправить», вы соглашаетесь с политикой обработки
+							персональных данных.
+						</span>
+					</form>
+				</div>
+			</div>
 		</section>
 	);
 }
---
+++ src/images/icons/ArrowIcon.tsx
@@ -0,0 +1,19 @@
+export default function ArrowIcon() {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+		>
+			<path
+				d="M7 17L17 7M17 7H9M17 7V15"
+				stroke="white"
+				strokeWidth="2"
+				strokeLinecap="square"
+				strokeLinejoin="round"
+			/>
+		</svg>
+	);
+}
