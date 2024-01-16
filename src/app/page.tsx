bd15e5ba320911347a3640b1ca831bafe5f4b916
ignateva-darya@mail.ru <dari.komarenko@gmail.com>
2024-01-16 11:31:27 +0300

    fix(fixed-error-page-styles): упростила структуру компонента и его стили, поменяла фоновую картинку

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import styles from './style.module.scss';
 
+//import Error from '@/components/ErrorPage';
+
 export default function Home() {
 	return <main className={styles['.main']}>Hello world!</main>;
+	//return <Error/>
 }
--- src/components/ErrorPage/index.tsx
+++ src/components/ErrorPage/index.tsx
@@ -4,22 +4,10 @@ import styles from './styles.module.scss';
 
 export default function Error() {
 	return (
-		<main>
-			<div className={styles['first-part']}>
-				<section className={styles['not-found']}>
-					<h1 className={styles['not-found__title']}>404</h1>
-				</section>
-			</div>
-			<div className={styles['second-part']}>
-				<p className={styles['not-found__description']}>
-					Такой страницы не существует
-				</p>
-				<div className={styles['not-found__back-link']}>
-					<button className={styles['not-found__button']} type="button">
-						Вернуться на главную
-					</button>
-				</div>
-			</div>
-		</main>
+		<div className={styles['container']}>
+			<h1>404</h1>
+			<p>Такой страницы не существует.</p>
+			<button>Вернуться на главную</button>
+		</div>
 	);
 }
