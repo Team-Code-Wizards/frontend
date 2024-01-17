cca749bd9d7ac1b4e18ff1b3e26115b26038e9ac
Egor Krasilnikov <krasilnikov19@gmail.com>
2024-01-17 14:10:55 +0300

    feat: создал компонент Portfolio.tsx

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,7 +1,10 @@
+import Portfolio from '../Portfolio';
 import styles from './style.module.scss';
 
 export default function MainPage() {
 	return (
-		<main className={styles.main}>{/* Подключаем свои компоненты тут */}</main>
+		<main className={styles.main}>
+			{/* Подключаем свои компоненты тут */ <Portfolio />}
+		</main>
 	);
 }
---
+++ src/components/Portfolio/index.tsx
@@ -0,0 +1,9 @@
+import style from './style.module.scss';
+
+export default function Portfolio() {
+	return (
+		<div className={style['card']}>
+			<div>fadsfdd</div>
+		</div>
+	);
+}
