d12a67c1eb4b7e0544a0498d8d77236594226647
JackKerouac1613 <133998734+JackKerouac1613@users.noreply.github.com>
2024-01-18 19:35:42 +0300

    feat: подгоняет блок Choice и Modal под PP

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,27 +1,10 @@
-import ComponentsContainer from '../ComponentsContainer';
-import Choice from '../Сhoice';
+import Modal from '../Modal';
 import styles from './style.module.scss';
 
 export default function MainPage() {
 	return (
 		<main className={styles.main}>
-			<ComponentsContainer>
-				<div
-					style={{
-						height: '200px',
-						width: '100vw',
-					}}
-				/>
-			</ComponentsContainer>
-			<Choice />
-			<ComponentsContainer>
-				<div
-					style={{
-						height: '200px',
-						width: '100vw',
-					}}
-				/>
-			</ComponentsContainer>
+			<Modal />
 		</main>
 	);
 }
--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -8,12 +8,12 @@ export default function Modal() {
 		<div className={styles['modal-back']}>
 			<div className={styles['modal']}>
 				<h2 className={styles['modal__title']}>Заявка на создание сайта</h2>
-				<div className={styles['modal__wrapper']}>
+				<form className={styles['modal__form']}>
 					{data.map((item) => {
 						return <ModalItem key={item.title} item={item} />;
 					})}
 					<button className={styles['modal__btn']}>Отправить</button>
-				</div>
+				</form>
 			</div>
 		</div>
 	);
