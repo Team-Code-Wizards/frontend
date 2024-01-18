ccfcbc65abbbba2223d8aa1affa143f67d89ac26
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-01-18 21:26:01 +0600

    fix: обновить стили после мержа из develop

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -4,34 +4,36 @@ import styles from './style.module.scss';
 
 export default function Services() {
 	return (
-		<div className={styles['services']}>
-			<h2 className={styles['services__title']}>Услуги</h2>
-			<div className={styles['services__cards']}>
-				{services.map((service) => (
-					<div key={service.title} className={styles['service-card']}>
-						<div className={styles['service-card__content']}>
-							<h4>{service.title}</h4>
-							{service.content.map((text, ind) => (
-								<p key={ind}>{text}</p>
-							))}
-							<span className={styles['service-card__prices']}>
-								<p className={styles['service-card__prices_pre']}>от</p>
-								<p className={styles['service-card__prices_new']}>
-									{service.newPrice}$
-								</p>
-								<p className={styles['service-card__prices_old']}>
-									{service.oldPrice}$
-								</p>
+		<div className={styles.wrapper}>
+			<div className={styles['services']}>
+				<h2 className={styles['services__title']}>Услуги</h2>
+				<div className={styles['services__cards']}>
+					{services.map((service) => (
+						<div key={service.title} className={styles['service-card']}>
+							<div className={styles['service-card__content']}>
+								<h4>{service.title}</h4>
+								{service.content.map((text, ind) => (
+									<p key={ind}>{text}</p>
+								))}
+								<span className={styles['service-card__prices']}>
+									<p className={styles['service-card__prices_pre']}>от</p>
+									<p className={styles['service-card__prices_new']}>
+										{service.newPrice}$
+									</p>
+									<p className={styles['service-card__prices_old']}>
+										{service.oldPrice}$
+									</p>
+								</span>
+							</div>
+							<span className={styles['service-card__btns']}>
+								<button className={styles['service-card__btns_order']}>
+									Заказать
+								</button>
+								<span className={styles['service-card__btns_ask']}>?</span>
 							</span>
 						</div>
-						<span className={styles['service-card__btns']}>
-							<button className={styles['service-card__btns_order']}>
-								Заказать
-							</button>
-							<span className={styles['service-card__btns_ask']}>?</span>
-						</span>
-					</div>
-				))}
+					))}
+				</div>
 			</div>
 		</div>
 	);
