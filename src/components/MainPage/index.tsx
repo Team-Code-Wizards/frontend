38b2243e206f8a7ce467ba1e73b1497439547946
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2024-01-18 15:37:39 +0300

    feat: изменить расположение Политики конфиденциальности для отображения отдельной страницей

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,11 +1,7 @@
-import PrivacyPolicy from '@/components/PrivacyPolicy';
-
 import styles from './style.module.scss';
 
 export default function MainPage() {
 	return (
-		<main className={styles.main}>
-			<PrivacyPolicy />
-		</main>
+		<main className={styles.main}>{/* Подключаем свои компоненты тут */}</main>
 	);
 }
