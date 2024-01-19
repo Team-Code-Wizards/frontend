55f671ab1cd5c48ec65c974552150ba2744c57b5
sariunar <rinchinovasariuna@gmail.com>
2024-01-19 14:26:36 +0800

    feat(add header): add header

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,7 +1,11 @@
+import Header from '../Header';
 import styles from './style.module.scss';
 
 export default function MainPage() {
 	return (
-		<main className={styles.main}>{/* Подключаем свои компоненты тут */}</main>
+		<main className={styles.main}>
+			{/* Подключаем свои компоненты тут */}
+			<Header />
+		</main>
 	);
 }
