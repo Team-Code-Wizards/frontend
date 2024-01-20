74c7bff8f5b743dc72354f6c861992490f310caa
JackKerouac1613 <133998734+JackKerouac1613@users.noreply.github.com>
2024-01-20 19:09:41 +0300

    feat: изменяет размеры модального окна

--- src/images/modal/IconClose.tsx
+++ src/images/modal/IconClose.tsx
@@ -1,14 +1,14 @@
 export default function IconClose() {
 	return (
 		<svg
-			width="25"
-			height="25"
-			viewBox="0 0 25 25"
+			width="20"
+			height="20"
+			viewBox="0 0 20 20"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
 		>
 			<path
-				d="M16.6673 16.6653L12.5007 12.4987M12.5007 12.4987L8.33398 8.33203M12.5007 12.4987L16.6673 8.33203M12.5007 12.4987L8.33398 16.6654"
+				d="M13.3327 13.3327L9.99936 9.99936M9.99936 9.99936L6.66602 6.66602M9.99936 9.99936L13.3327 6.66602M9.99936 9.99936L6.66602 13.3327"
 				stroke="white"
 				strokeWidth="2"
 				strokeLinecap="round"
