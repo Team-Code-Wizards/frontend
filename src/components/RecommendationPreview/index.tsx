bdcd5d3395139a8c459cfecb1f7886774f69baed
ignateva-darya@mail.ru <dari.komarenko@gmail.com>
2024-01-22 09:19:06 +0300

    change pictures to png

--- src/components/RecommendationPreview/index.tsx
+++ src/components/RecommendationPreview/index.tsx
@@ -1,25 +1,19 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-import IRecommendationPreview from '@/interfaces/RecommendationPreview';
+import IRecommendationPreview from "@/interfaces/RecommendationPreview";
 
-import styles from './style.module.scss';
+import styles from "./style.module.scss";
 
 export default function RecommendationPreview({
-	image,
-	width,
-	height,
+  image,
+  width,
+  height,
 }: IRecommendationPreview) {
-	return (
-		<div className={styles['card__container']}>
-			<div className={styles['card__image']}>
-				<Image
-					src={image}
-					alt="video-preview"
-					width={width}
-					height={height}
-					style={{ position: 'absolute', left: '0px', top: '-4px' }}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles["card__container"]}>
+      <div className={styles["card__image"]}>
+        <Image src={image} alt="video-preview" width={width} height={height} />
+      </div>
+    </div>
+  );
 }
--- src/constants/Recommendations/index.ts
+++ src/constants/Recommendations/index.ts
@@ -1,36 +1,36 @@
-import IRecomendationPreview from '@/interfaces/RecommendationPreview';
+import IRecomendationPreview from "@/interfaces/RecommendationPreview";
 
-import circle from '../../images/recommendations/circle.svg';
-import secondSlide from '../../images/recommendations/slide-center.jpg';
-import firstSlide from '../../images/recommendations/slide-left.jpg';
-import thirdSlide from '../../images/recommendations/видео 1.jpg';
+import circle from "../../images/recommendations/circle.svg";
+import centerSlide from "../../images/recommendations/center-slide.png";
+import leftSlide from "../../images/recommendations/left-slide.png";
+import rightSlide from "../../images/recommendations/right-slide.png";
 
 export const recommendationsPreviews: IRecomendationPreview[] = [
-	{
-		id: 0,
-		title: '',
-		info: '',
-		image: firstSlide,
-		tag: '',
-		width: 300,
-		height: 200,
-	},
-	{
-		id: 1,
-		title: 'Рент-зона',
-		info: 'Аренда строительной техники',
-		image: secondSlide,
-		tag: circle,
-		width: 712,
-		height: 423,
-	},
-	{
-		id: 2,
-		title: '',
-		info: '',
-		image: thirdSlide,
-		tag: '',
-		width: 300,
-		height: 200,
-	},
+  {
+    id: 0,
+    title: "",
+    info: "",
+    image: leftSlide,
+    tag: "",
+    width: 525,
+    height: 314,
+  },
+  {
+    id: 1,
+    title: "Рент-зона",
+    info: "Аренда строительной техники",
+    image: centerSlide,
+    tag: circle,
+    width: 712,
+    height: 423,
+  },
+  {
+    id: 2,
+    title: "",
+    info: "",
+    image: rightSlide,
+    tag: "",
+    width: 621,
+    height: 314,
+  },
 ];
