c97ae9125d56df3e79e7c3d7c8a4df4413e38020
Egor Krasilnikov <krasilnikov19@gmail.com>
2024-01-22 12:00:32 +0300

    feat(promo): иницилизировал компонент Promo

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -3,9 +3,10 @@ import ComponentsContainer from '../ComponentsContainer';
 import Contacts from '../Contacts';
 import Footer from '../Footer';
 import Guarantee from '../Guarantee/index';
-import Header from '../Header';
+// import Header from '../Header';
 import Interaction from '../Interaction/index';
 import Portfolio from '../Portfolio';
+import Promo from '../Promo';
 import Services from '../Services';
 import StageCreate from '../StageCreate';
 import TechStack from '../TechStack';
@@ -15,9 +16,10 @@ import styles from './style.module.scss';
 export default function MainPage() {
 	return (
 		<>
-			<Header />
+			{/* <Header /> */}
 			<main className={styles.main}>
 				{/*<Modal />*/}
+				<Promo />
 				<ComponentsContainer>
 					<AboutUs />
 					<Services />
---
+++ src/components/Promo/index.tsx
@@ -0,0 +1,11 @@
+import Form from './Form';
+import style from './style.module.scss';
+
+export default function Promo() {
+	return (
+		<section className={style['promo__container']}>
+			<div></div>
+			<Form />
+		</section>
+	);
+}
