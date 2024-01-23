25f34441f21a084aa13a1e050c9c43fbb8842659
JackKerouac1613 <133998734+JackKerouac1613@users.noreply.github.com>
2024-01-23 21:22:28 +0300

    feat: добавляет адаптивность блоку footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -14,13 +14,18 @@ const Footer = (): React.ReactElement => {
 	return (
 		<footer className={styles['footer']}>
 			<div className={styles['footer__row']}>
-				<div>
+				<div className={styles['footer__row-first']}>
 					<p>Политика конфиденциальности</p>
 					<p>© Code Wizards, 2023</p>
 				</div>
 				<div>
 					<a href="tel:+79504241338">+7 (950) 424-13-38</a>
 					<a href="mailto:email@code-wizards.team">email@code-wizards.team</a>
+					<Image
+						src={LogoText}
+						alt="Логотип Code Wizards"
+						className={styles['footer__image-text-adaptive']}
+					/>
 				</div>
 			</div>
 
@@ -36,13 +41,13 @@ const Footer = (): React.ReactElement => {
 					className={styles['footer__image-text']}
 				/>
 				<div className={styles['footer__social-group']}>
-					<a href="#">
+					<a href="#" className={styles['footer__icon_telegram']}>
 						<TelegramIcon />
 					</a>
-					<a href="#">
+					<a href="#" className={styles['footer__icon_whatsapp']}>
 						<WhatsappIcon />
 					</a>
-					<a href="#">
+					<a href="#" className={styles['footer__icon_viber']}>
 						<ViberIcon />
 					</a>
 				</div>
