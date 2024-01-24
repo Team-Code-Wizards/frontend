7c6f442020f236fbf0c4345de70a85edda1b5269
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2024-01-24 19:24:53 +0300

    feat: изменить компонент NotFound для десктоп, скорректировать стили

--- src/components/notFound/index.tsx
+++ src/components/notFound/index.tsx
@@ -2,14 +2,14 @@ import styles from './styles.module.scss';
 
 export default function NotFound() {
 	return (
-		<div className={styles['container']}>
-			<h1 className={styles['container_name']}>404</h1>
-			<p className={styles['container_description']}>
-				Такой страницы не существует.
-			</p>
-			<button className={styles['container_button']}>
-				Вернуться на главную
-			</button>
+		<div className={styles['not-found']}>
+			<div className={styles['not-found__container']}>
+				<h1 className={styles['name']}>404</h1>
+				<p className={styles['text']}>Такой страницы не существует.</p>
+				<button type="button" className={styles['btn']}>
+					<a href="/">Вернуться на главную</a>
+				</button>
+			</div>
 		</div>
 	);
 }
