5b4e4e62b4a2db3f15976ed6a02e7219b1207ccf
Egor Krasilnikov <krasilnikov19@gmail.com>
2024-01-25 00:20:30 +0300

    style(about-us): update about-us component

--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -7,39 +7,37 @@ import style from './style.module.scss';
 export default function AboutUs() {
 	return (
 		<section id="about-us" className={style['about-us']}>
-			<div className={style['about-us__container']}>
-				<h2>О нас</h2>
-				<h3>
-					Мы – коллектив творческих умов, готовых воплощать <br /> ваши идеи в
-					цифровую реальность.
-				</h3>
-				<div className={style['about-us__container_content']}>
-					<Image
-						src={laptopImage}
-						className={style['image']}
-						alt={'laptop-image'}
-					/>
-					<div className={style['about-us__container_content_text']}>
-						<div className={style['about-us__container_content_text_goals']}>
-							<p>5 лет опыта</p>
-							<p>Внедрение инноваций</p>
-							<p>Современные технологии</p>
-						</div>
-						<div className={style['about-us__container_content_text_resume']}>
-							<p>
-								Мы начали как группа энтузиастов, которые верили в силу креатива
-								и технологий. С течением времени наша страсть к веб-разработке
-								превратилась в виртуозное искусство создания сайтов для малого и
-								среднего бизнеса. Мы используем передовые решения для того,
-								чтобы ваш сайт не только соответствовал, но и опережал свое
-								время.
-							</p>
-							<p>
-								Пять лет опыта — это не просто цифры, это накопленные знания,
-								глубокое понимание отрасли и надежная платформа для вашего
-								успеха.
-							</p>
-						</div>
+			<h2 className={style['about-us__h2-title']}>О нас</h2>
+			<h3 className={style['about-us__h3-title']}>
+				Мы – коллектив творческих умов, готовых воплощать ваши идеи в цифровую{' '}
+				реальность.
+			</h3>
+			<div className={style['about-us__main']}>
+				<Image
+					src={laptopImage}
+					className={style['about-us__image']}
+					alt={'laptop-image'}
+				/>
+				<div className={style['about-us__goals']}>
+					<ul className={style['about-us__goals-list']}>
+						<li className={style['goals-list__item']}>5 лет опыта</li>
+						<li className={style['goals-list__item']}>Внедрение инноваций</li>
+						<li className={style['goals-list__item']}>
+							Современные технологии
+						</li>
+					</ul>
+					<div className={style['about-us__goals-resume']}>
+						<p className={style['goals-resume__item']}>
+							Мы начали как группа энтузиастов, которые верили в силу креатива и
+							технологий. С течением времени наша страсть к веб-разработке
+							превратилась в виртуозное искусство создания сайтов для малого и
+							среднего бизнеса. Мы используем передовые решения для того, чтобы
+							ваш сайт не только соответствовал, но и опережал свое время.
+						</p>
+						<p className={style['goals-resume__item']}>
+							Пять лет опыта — это не просто цифры, это накопленные знания,
+							глубокое понимание отрасли и надежная платформа для вашего успеха.
+						</p>
 					</div>
 				</div>
 			</div>
