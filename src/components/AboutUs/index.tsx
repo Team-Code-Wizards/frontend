c215c37208fc9917ee45d1d0de750ccc3e9f7a21
Egor Krasilnikov <krasilnikov19@gmail.com>
2024-01-24 14:33:21 +0300

    feat(tsconfig): added alias for images

--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -1,26 +1,31 @@
 import Image from 'next/image';
 
-import laptopImage from '../../../public/images/aboutUs/laptop-image.png';
-import styles from './style.module.scss';
+import laptopImage from '&/images/aboutUs/laptop-image.png';
+
+import style from './style.module.scss';
 
 export default function AboutUs() {
 	return (
-		<section id="about-us" className={styles['about-us']}>
-			<div className={styles['about-us__container']}>
+		<section id="about-us" className={style['about-us']}>
+			<div className={style['about-us__container']}>
 				<h2>О нас</h2>
 				<h3>
 					Мы – коллектив творческих умов, готовых воплощать <br /> ваши идеи в
 					цифровую реальность.
 				</h3>
-				<div className={styles['about-us__container_content']}>
-					<Image src={laptopImage} alt={'Laptop image'} />
-					<div className={styles['about-us__container_content_text']}>
-						<div className={styles['about-us__container_content_text_goals']}>
+				<div className={style['about-us__container_content']}>
+					<Image
+						src={laptopImage}
+						className={style['image']}
+						alt={'laptop-image'}
+					/>
+					<div className={style['about-us__container_content_text']}>
+						<div className={style['about-us__container_content_text_goals']}>
 							<p>5 лет опыта</p>
 							<p>Внедрение инноваций</p>
 							<p>Современные технологии</p>
 						</div>
-						<div className={styles['about-us__container_content_text_resume']}>
+						<div className={style['about-us__container_content_text_resume']}>
 							<p>
 								Мы начали как группа энтузиастов, которые верили в силу креатива
 								и технологий. С течением времени наша страсть к веб-разработке
