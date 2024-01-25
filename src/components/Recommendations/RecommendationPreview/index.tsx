c9e3e5eb280ed90d6e5258b95e515feb735d23f9
Oleg Smitkevich <o.smitkevich@gmail.com>
2024-01-26 02:13:26 +0300

    feat: адаптив копмонента Отзывы и рекомендации

--- src/components/Recommendations/RecommendationPreview/index.tsx
+++ src/components/Recommendations/RecommendationPreview/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 
+import circle from '../../../../public/images/recommendations/circle.svg';
 import IRecommendationPreview from './interface';
 import styles from './style.module.scss';
 
@@ -7,7 +8,28 @@ export default function RecommendationPreview({
 	image,
 	className,
 }: IRecommendationPreview) {
-	return (
+	return className === '_center' ? (
+		<div className={styles['main-card-container']}>
+			<Image
+				src={image}
+				alt="video-preview"
+				className={styles[`card-image${className}`]}
+			/>
+			<div className={styles['card__details']}>
+				<Image
+					className={styles['card__details_circle']}
+					src={circle}
+					alt="circle"
+				/>
+				<div className={styles['card__details__container']}>
+					<h3 className={styles['card__details_title']}>Рент-зона</h3>
+					<h4 className={styles['card__details_information']}>
+						Аренда строительной техники
+					</h4>
+				</div>
+			</div>
+		</div>
+	) : (
 		<Image
 			src={image}
 			alt="video-preview"
