09b6d54e311fccaa6a6b5c6758a1c7118271c4c4
Ilvina <ilvrakh@gmail.com>
2024-01-26 20:54:13 +0500

    feat: подготовила к пуллу из develop

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -7,6 +7,7 @@ import Guarantee from '../Guarantee/index';
 import Interaction from '../Interaction/index';
 import Navbar from '../Navbar';
 import Portfolio from '../Portfolio';
+import ProjectDescriptionModal from '../ProjectDescriptionModal';
 import Promo from '../Promo';
 import Recommendations from '../Recommendations';
 import Services from '../Services';
@@ -22,7 +23,7 @@ export default function MainPage() {
 			<Navbar />
 			<Promo />
 			<ComponentsContainer>
-				{/*<ProjectDescriptionModal />*/}
+				<ProjectDescriptionModal />
 				<AboutUs />
 				<Services />
 				<Portfolio />
