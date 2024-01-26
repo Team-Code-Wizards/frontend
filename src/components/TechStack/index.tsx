ad1ba826ce335f9fe893a81c7ed1b9d9af0972c1
Natalia Lebedeva <nataliadaveng@gmail.com>
2024-01-26 23:21:43 +0600

    feat: настроить стили под ПП для экранов от 1440px

--- src/components/TechStack/index.tsx
+++ src/components/TechStack/index.tsx
@@ -7,30 +7,32 @@ import style from './style.module.scss';
 
 export default function TechStack() {
 	return (
-		<section className={style['tech-stack__container']}>
-			<div className={style['tech-stack__head']}>
-				<div className={style['tech-stack__banner']}>
-					<h3 className={style['tech-stack__title']}>
-						Наш технический арсенал
-					</h3>
-					<p className={style['tech-stack__subtitle']}>
-						Взгляните на то, что лежит в основе нашей творческой энергии:
-					</p>
+		<section className={style['tech-stack']}>
+			<div className={style['tech-stack__container']}>
+				<div className={style['tech-stack__head']}>
+					<div className={style['tech-stack__banner']}>
+						<h3 className={style['tech-stack__title']}>
+							Наш технический арсенал
+						</h3>
+						<p className={style['tech-stack__subtitle']}>
+							Взгляните на то, что лежит в основе нашей творческой энергии:
+						</p>
+					</div>
+					<div className={style['tech-stack__short-description']}>
+						<StarIcon />
+						<p>
+							В создании выдающихся веб-решений мы вкладываем не только талант и
+							страсть, но и прибегаем к передовым технологиям. наш блестящий
+							стек технологий обеспечивает несравненное качество и
+							производительность вашего сайта.
+						</p>
+					</div>
 				</div>
-				<div className={style['tech-stack__short-description']}>
-					<StarIcon />
-					<p>
-						В создании выдающихся веб-решений мы вкладываем не только талант и
-						страсть, но и прибегаем к передовым технологиям. наш блестящий стек
-						технологий обеспечивает несравненное качество и производительность
-						вашего сайта.
-					</p>
+				<div className={style['tech-stack__main']}>
+					{cardLines.map((el) => (
+						<CardLine key={uuidv4()} {...el} />
+					))}
 				</div>
-			</div>
-			<div className={style['tech-stack__main']}>
-				{cardLines.map((el) => (
-					<CardLine key={uuidv4()} {...el} />
-				))}
 			</div>
 		</section>
 	);
