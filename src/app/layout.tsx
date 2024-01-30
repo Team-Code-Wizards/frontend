403cd6ad6cbe1261e70f930b427c4f27491fea49
Владислав Корнюхов <63837534+Govard9@users.noreply.github.com>
2024-01-30 22:31:44 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="ru" /* className={inter.className} */>
+		<html lang="ru">
 			<Head>
 				<link rel="icon" href="/favicon.ico" sizes="any" />
 				<!-- Yandex.Metrika counter -->
