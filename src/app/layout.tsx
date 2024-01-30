f1dea5de4e9088b04a5f71d021d235b4247335c3
Владислав Корнюхов <63837534+Govard9@users.noreply.github.com>
2024-01-30 22:25:59 +0700

    Update layout.tsx
    feat: добавил яндекс метрику

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,23 @@ export default function RootLayout({
 		<html lang="ru" /* className={inter.className} */>
 			<Head>
 				<link rel="icon" href="/favicon.ico" sizes="any" />
+				<!-- Yandex.Metrika counter -->
+				<script type="text/javascript" >
+				   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+				   m[i].l=1*new Date();
+				   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+				   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+				   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+				
+				   ym(96308291, "init", {
+					clickmap:true,
+					trackLinks:true,
+					accurateTrackBounce:true,
+					webvisor:true
+				   });
+				</script>
+				<noscript><div><img src="https://mc.yandex.ru/watch/96308291" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+				<!-- /Yandex.Metrika counter -->
 			</Head>
 			<body className="body">{children}</body>
 		</html>
