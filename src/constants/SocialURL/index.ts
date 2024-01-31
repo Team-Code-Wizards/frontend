99c39f50c7a6fa614960e337994273767b89fcaa
OleG Smitkevich <114614095+OGSmit@users.noreply.github.com>
2024-01-31 16:58:11 +0300

    Update index.ts

--- src/constants/SocialURL/index.ts
+++ src/constants/SocialURL/index.ts
@@ -1,7 +1,7 @@
 const SOCIAL_URL = {
 	watsup: 'https://api.whatsapp.com/send?phone=79504241338',
 	telegram: 'https://t.me/govard9',
-	viber: 'https://chat?number=%2B79504241338',
+	viber: 'viber://chat?number=%2B79504241338',
 };
 
 export default SOCIAL_URL;
