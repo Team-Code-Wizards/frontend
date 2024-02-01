ed7b4006083866da61b4db16bbd636063a2f11e1
ignateva-darya@mail.ru <dari.komarenko@gmail.com>
2024-02-01 09:19:25 +0300

    fix(popup-fix): убрала закомментированные компоненты из MainPage

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,13 +1,20 @@
 import AboutUs from '../AboutUs';
 import ComponentsContainer from '../ComponentsContainer';
 import Contacts from '../Contacts';
+import Faq from '../Faq';
 import Footer from '../Footer';
 import Guarantee from '../Guarantee/index';
 import Interaction from '../Interaction/index';
 import Navbar from '../Navbar';
+import Portfolio from '../Portfolio';
 //import ProjectDescriptionModal from "../ProjectDescriptionModal";
-import Popup from '../Popup';
+//import Popup from '../Popup';
 import Promo from '../Promo';
+import Recommendations from '../Recommendations';
+import Services from '../Services';
+import StageCreate from '../StageCreate';
+import TechStack from '../TechStack';
+import Choice from '../Сhoice';
 import styles from './style.module.scss';
 
 export default function MainPage() {
@@ -18,19 +25,19 @@ export default function MainPage() {
 			<Promo />
 			<ComponentsContainer>
 				{/* <ProjectDescriptionModal /> */}
-				<Popup isSuccess={true} />
+				{/* <Popup isSuccess={true} /> */}
 				<AboutUs />
-				{/* <Services /> */}
-				{/* <Portfolio /> */}
+				<Services />
+				<Portfolio />
 			</ComponentsContainer>
-			{/* <StageCreate /> */}
-			{/* <Recommendations /> */}
-			{/* <Choice /> */}
+			<StageCreate />
+			<Recommendations />
+			<Choice />
 			<Guarantee />
 			<Interaction />
-			{/* <TechStack /> */}
+			<TechStack />
 			<ComponentsContainer>
-				{/* <Faq /> */}
+				<Faq />
 				<Contacts />
 				<Footer />
 			</ComponentsContainer>
