c6b14b244317c78314f74054be8875b3d4e1924d
JackKerouac1613 <133998734+JackKerouac1613@users.noreply.github.com>
2024-02-02 16:19:27 +0300

    feat: добавляет начальные настройки слайдера

--- src/components/Recommendations/index.tsx
+++ src/components/Recommendations/index.tsx
@@ -1,4 +1,9 @@
+'use client';
+
 import { recommendationsPreviews } from '@/constants/Recommendations';
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/scss';
 import { v4 as uuidv4 } from 'uuid';
 
 import ArrowChevronIcon from '../../../public/images/icons/ArrowChevronIcon';
@@ -14,20 +19,46 @@ export default function Recommendations() {
 						Отзывы и рекомендации
 					</h2>
 					<div className={styles['recommendations__arrows']}>
-						<button className={styles['arrow-btn']} type="button">
+						<button
+							className={styles['arrow-btn']}
+							type="button"
+							id="swiper-back"
+						>
 							<ArrowChevronIcon direction="left" />
 						</button>
-						<button className={styles['arrow-btn']} type="button">
+						<button
+							className={styles['arrow-btn']}
+							type="button"
+							id="swiper-forward"
+						>
 							<ArrowChevronIcon direction="right" />
 						</button>
 					</div>
 				</div>
 
-				<div className={styles['card__container']}>
+				<Swiper
+					spaceBetween={50}
+					slidesPerView={3}
+					centeredSlides={true}
+					loop={true}
+					className={styles['card__container']}
+					modules={[Navigation, Pagination]}
+					navigation={{
+						nextEl: '#swiper-forward',
+						prevEl: '#swiper-back',
+					}}
+				>
 					{recommendationsPreviews.map((item) => {
-						return <RecommendationPreview key={uuidv4()} {...item} />;
+						return (
+							<SwiperSlide
+								key={uuidv4()}
+								className={styles['recommendations-swiper-slide']}
+							>
+								<RecommendationPreview key={uuidv4()} {...item} />
+							</SwiperSlide>
+						);
 					})}
-				</div>
+				</Swiper>
 			</div>
 		</section>
 	);
