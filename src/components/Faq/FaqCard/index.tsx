35b26f328a7b17f21b6fd945cc44ae5bc2de69be
ignateva-darya@mail.ru <dari.komarenko@gmail.com>
2024-02-06 12:27:10 +0300

    feat(faq-logic): добавила состояния для FAQ

--- src/components/Faq/FaqCard/index.tsx
+++ src/components/Faq/FaqCard/index.tsx
@@ -1,12 +1,23 @@
+'use client';
+
+import { useState } from 'react';
+
 import PlusIcon from '../../../../public/images/icons/Plus';
 import { IFaqCardProps } from './interface';
 import styles from './styles.module.scss';
 
 function FaqCard({ item }: IFaqCardProps) {
+	const [isOpen, setIsOpen] = useState(false);
+	function toggleDescription() {
+		setIsOpen(!isOpen);
+	}
 	return (
 		<li className={styles['faq-card']}>
-			<details>
-				<summary className={styles['faq-card__header']}>
+			<details open={isOpen}>
+				<summary
+					className={styles['faq-card__header']}
+					onClick={toggleDescription}
+				>
 					<h4 className={styles['faq-card__title']}>{item.title}</h4>
 					<PlusIcon />
 				</summary>
