74612f14ebe1e0d4c80b6667b53a0902d6893844
ignateva-darya@mail.ru <dari.komarenko@gmail.com>
2024-02-06 21:18:51 +0300

    feat(faq-logic): добавила логику открытия ответов на вопросы

--- src/components/Faq/FaqCard/index.tsx
+++ src/components/Faq/FaqCard/index.tsx
@@ -1,23 +1,37 @@
-'use client';
-
-import { useState } from 'react';
-
 import PlusIcon from '../../../../public/images/icons/Plus';
 import { IFaqCardProps } from './interface';
+// import { IFaqData } from "@/constants/Faq/type";
 import styles from './styles.module.scss';
 
-function FaqCard({ item }: IFaqCardProps) {
-	const [isOpen, setIsOpen] = useState(false);
-	function toggleDescription() {
-		setIsOpen(!isOpen);
-	}
+// import { useEffect, useState } from "react";
+
+function FaqCard({
+	item,
+	handleClick,
+}: {
+	item: IFaqCardProps;
+	handleClick: (question: IFaqCardProps) => void;
+}) {
+	// const [open, setOpen] = useState(item.active);
+	//   useEffect(() => {
+	//     console.log(item);
+	//   }, [item]);
+
+	//   const onToggle = () => {
+	//     setOpen(!open);
+	//   };
+
 	return (
 		<li className={styles['faq-card']}>
-			<details open={isOpen}>
-				<summary
-					className={styles['faq-card__header']}
-					onClick={toggleDescription}
-				>
+			<details
+				open={item.active}
+				onClick={(e) => {
+					e.preventDefault();
+					//          onToggle();
+					handleClick(item);
+				}}
+			>
+				<summary className={styles['faq-card__header']} onClick={() => {}}>
 					<h4 className={styles['faq-card__title']}>{item.title}</h4>
 					<PlusIcon />
 				</summary>
