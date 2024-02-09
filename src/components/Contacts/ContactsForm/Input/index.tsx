18b2ef06027dfd93f7da11d40484c7707a684ac2
Ilvina <ilvrakh@gmail.com>
2024-02-09 14:26:56 +0500

    feat: переписывание инпута

--- src/components/Contacts/ContactsForm/Input/index.tsx
+++ src/components/Contacts/ContactsForm/Input/index.tsx
@@ -1,15 +1,117 @@
-// с контекстоm
-import { useEffect, useState } from 'react';
-import { useFormContext } from 'react-hook-form';
+// // с контекстоm
+// import { useEffect, useState } from 'react';
+// import { useFormContext } from 'react-hook-form';
+// import ClearInputButton from './ClearInputButton';
+// import IInput from './interface';
+// import styles from './styles.module.scss';
+// // type Inputs = {
+// // 	clientTel: string | undefined;
+// // 	clientEmail: string | undefined;
+// // };
+// export default function Input({
+// 	id,
+// 	className,
+// 	type,
+// 	name,
+// 	placeholder,
+// 	value,
+// 	accept,
+// 	inputType,
+// 	labelText,
+// 	labelStyleKey,
+// 	wrapperStyleKey,
+// 	clearInputStyleKey,
+// 	formState,
+// }: IInput) {
+// 	const [isActive, setIsActive] = useState(false);
+// 	const { register } = useFormContext() ?? {};
+// 	// const { errors } = formState;
+// 	if (!register) return null;
+// 	if (inputType === 'textarea')
+// 		return (
+// 			<span className={wrapperStyleKey && styles[wrapperStyleKey]}>
+// 				<textarea
+// 					{...register('message')}
+// 					className={className && styles[className]}
+// 					// name={name}
+// 					placeholder={placeholder}
+// 					onFocus={() => setIsActive(true)}
+// 					onBlur={() => setIsActive(false)}
+// 				/>
+// 				<ClearInputButton styleKey={clearInputStyleKey} isHidden={!isActive} />
+// 			</span>
+// 		);
+// 	if (inputType === 'inputFile')
+// 		return (
+// 			<>
+// 				<input
+// 					{...register('file')}
+// 					className={className && styles[className]}
+// 					type={type}
+// 					id="file"
+// 					// name={name}
+// 					accept={accept}
+// 				/>
+// 				<label
+// 					className={labelStyleKey && styles[labelStyleKey]}
+// 					htmlFor="file"
+// 				>
+// 					{labelText}
+// 				</label>
+// 			</>
+// 		);
+// 	return (
+// 		<span className={wrapperStyleKey && styles[wrapperStyleKey]}>
+// 			{labelText && (
+// 				<label className={labelStyleKey && styles[labelStyleKey]} htmlFor={id}>
+// 					{labelText}
+// 				</label>
+// 			)}
+// 			{name === 'clientName' && (
+// 				<input
+// 					{...register('name')}
+// 					type={type}
+// 					className={className && styles[className]}
+// 					id={id}
+// 					placeholder={placeholder}
+// 					value={value}
+// 					onFocus={() => setIsActive(true)}
+// 					onBlur={() => setIsActive(false)}
+// 				/>
+// 			)}
+// 			{name === 'clientTel' && (
+// 				<input
+// 					{...register('tel')}
+// 					type={type}
+// 					className={className && styles[className]}
+// 					id={id}
+// 					placeholder={placeholder}
+// 					value={value}
+// 					onFocus={() => setIsActive(true)}
+// 					onBlur={() => setIsActive(false)}
+// 				/>
+// 			)}
+// 			{name === 'clientEmail' && (
+// 				<input
+// 					{...register('email')}
+// 					type={type}
+// 					className={className && styles[className]}
+// 					id={id}
+// 					placeholder={placeholder}
+// 					value={value}
+// 					onFocus={() => setIsActive(true)}
+// 					onBlur={() => setIsActive(false)}
+// 				/>
+// 			)}
+// 			<ClearInputButton styleKey={clearInputStyleKey} isHidden={!isActive} />
+// 		</span>
+// 	);
+// }
+import { useState } from 'react';
 
 import ClearInputButton from './ClearInputButton';
 import IInput from './interface';
 import styles from './styles.module.scss';
-
-// type Inputs = {
-// 	clientTel: string | undefined;
-// 	clientEmail: string | undefined;
-// };
 
 export default function Input({
 	id,
@@ -24,25 +126,15 @@ export default function Input({
 	labelStyleKey,
 	wrapperStyleKey,
 	clearInputStyleKey,
-	formState,
 }: IInput) {
 	const [isActive, setIsActive] = useState(false);
-	const { register } = useFormContext() ?? {};
-	// const { errors } = formState;
-
-	useEffect(() => {
-		console.log(formState);
-	}, [formState]);
-
-	if (!register) return null;
 
 	if (inputType === 'textarea')
 		return (
 			<span className={wrapperStyleKey && styles[wrapperStyleKey]}>
 				<textarea
-					{...register('message')}
 					className={className && styles[className]}
-					// name={name}
+					name={name}
 					placeholder={placeholder}
 					onFocus={() => setIsActive(true)}
 					onBlur={() => setIsActive(false)}
@@ -55,11 +147,10 @@ export default function Input({
 		return (
 			<>
 				<input
-					{...register('file')}
 					className={className && styles[className]}
 					type={type}
 					id="file"
-					// name={name}
+					name={name}
 					accept={accept}
 				/>
 				<label
@@ -70,6 +161,7 @@ export default function Input({
 				</label>
 			</>
 		);
+
 	return (
 		<span className={wrapperStyleKey && styles[wrapperStyleKey]}>
 			{labelText && (
@@ -77,120 +169,17 @@ export default function Input({
 					{labelText}
 				</label>
 			)}
-			{name === 'clientName' && (
-				<input
-					{...register('name')}
-					type={type}
-					className={className && styles[className]}
-					id={id}
-					placeholder={placeholder}
-					value={value}
-					onFocus={() => setIsActive(true)}
-					onBlur={() => setIsActive(false)}
-				/>
-			)}
-			{name === 'clientTel' && (
-				<input
-					{...register('tel')}
-					type={type}
-					className={className && styles[className]}
-					id={id}
-					placeholder={placeholder}
-					value={value}
-					onFocus={() => setIsActive(true)}
-					onBlur={() => setIsActive(false)}
-				/>
-			)}
-			{name === 'clientEmail' && (
-				<input
-					{...register('email')}
-					type={type}
-					className={className && styles[className]}
-					id={id}
-					placeholder={placeholder}
-					value={value}
-					onFocus={() => setIsActive(true)}
-					onBlur={() => setIsActive(false)}
-				/>
-			)}
+			<input
+				type={type}
+				className={className && styles[className]}
+				name={name}
+				id={id}
+				placeholder={placeholder}
+				value={value}
+				onFocus={() => setIsActive(true)}
+				onBlur={() => setIsActive(false)}
+			/>
 			<ClearInputButton styleKey={clearInputStyleKey} isHidden={!isActive} />
 		</span>
 	);
 }
-
-// import { useState } from 'react';
-
-// import ClearInputButton from './ClearInputButton';
-// import IInput from './interface';
-// import styles from './styles.module.scss';
-
-// export default function Input({
-// 	id,
-// 	className,
-// 	type,
-// 	name,
-// 	placeholder,
-// 	value,
-// 	accept,
-// 	inputType,
-// 	labelText,
-// 	labelStyleKey,
-// 	wrapperStyleKey,
-// 	clearInputStyleKey,
-// }: IInput) {
-// 	const [isActive, setIsActive] = useState(false);
-
-// 	if (inputType === 'textarea')
-// 		return (
-// 			<span className={wrapperStyleKey && styles[wrapperStyleKey]}>
-// 				<textarea
-// 					className={className && styles[className]}
-// 					name={name}
-// 					placeholder={placeholder}
-// 					onFocus={() => setIsActive(true)}
-// 					onBlur={() => setIsActive(false)}
-// 				/>
-// 				<ClearInputButton styleKey={clearInputStyleKey} isHidden={!isActive} />
-// 			</span>
-// 		);
-
-// 	if (inputType === 'inputFile')
-// 		return (
-// 			<>
-// 				<input
-// 					className={className && styles[className]}
-// 					type={type}
-// 					id="file"
-// 					name={name}
-// 					accept={accept}
-// 				/>
-// 				<label
-// 					className={labelStyleKey && styles[labelStyleKey]}
-// 					htmlFor="file"
-// 				>
-// 					{labelText}
-// 				</label>
-// 			</>
-// 		);
-
-// 	return (
-// 		<span className={wrapperStyleKey && styles[wrapperStyleKey]}>
-// 			{labelText && (
-// 				<label className={labelStyleKey && styles[labelStyleKey]} htmlFor={id}>
-// 					{labelText}
-// 				</label>
-// 			)}
-// 			<input
-// 				type={type}
-// 				className={className && styles[className]}
-// 				name={name}
-// 				id={id}
-// 				placeholder={placeholder}
-// 				value={value}
-// 				onFocus={() => setIsActive(true)}
-// 				onBlur={() => setIsActive(false)}
-// 			/>
-// 			<ClearInputButton styleKey={clearInputStyleKey} isHidden={!isActive} />
-// 		</span>
-// 	);
-// }
--- src/components/Contacts/ContactsForm/index.tsx
+++ src/components/Contacts/ContactsForm/index.tsx
@@ -1,82 +1,130 @@
-// // с контектсом 2
-// import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+import ClearInputButton from './Input/ClearInputButton';
 // import { FormProvider } from 'react-hook-form';
 // import { inputs } from '@/constants/Contacts';
-// import { yupResolver } from '@hookform/resolvers/yup';
-// import * as yup from 'yup';
 // import Input from './Input';
-// import IContactsFormProps from './interface';
-// import styles from './styles.module.scss';
-// export default function ContactsForm({ onSubmit }: IContactsFormProps) {
-// 	return (
-// 		<div className={styles['form-box']}>
-// 			<form className={styles['form']} onSubmit={onSubmit}>
-// 				{inputs.map((el) => (
-// 					<Input key={`key${el.id}`} {...el} />
-// 				))}
-// 				<span className={styles['form__notice']}>*В формате Документ Word</span>
-// 				<button className={styles['form__button']} type="submit">
-// 					Отправить
-// 				</button>
-// 				<span className={styles['form__notice']}>
-// 					Нажимая кнопку «отправить», вы соглашаетесь с политикой обработки
-// 					персональных данных.
-// 				</span>
-// 			</form>
-// 		</div>
-// 	);
-// }
-// С КОНТЕКСТОМ
-import { useForm } from 'react-hook-form';
-import { FormProvider } from 'react-hook-form';
-
-import { inputs } from '@/constants/Contacts';
-
-import Input from './Input';
 // import IContactsFormProps from './interface';
 import styles from './styles.module.scss';
 
-interface IFormInput {
-	name: string;
-	tel: number;
-	email: string;
-	message: string;
-	file: string;
-}
+export const schema = yup.object().shape({
+	clientName: yup.string().notRequired(),
+	clientTel: yup
+		.string()
+		.required('Введите номер телефона')
+		.matches(/^\+?\d{10,}$/g, 'Неверный формат номера телефона'),
+	clientEmail: yup
+		.string()
+		.required('Введите Email')
+		.matches(/.+@.+\..+/i, 'Неверный формат Email'),
+	clientMessage: yup.string().notRequired(),
+	clientFile: yup.mixed().notRequired(),
+});
+// interface IFormInput {
+// 	clientName: string;
+// 	clientTel: number;
+// 	clientEmail: string;
+// 	clientMessage: string;
+// 	clientFile: string;
+// }
 
 export default function ContactsForm() {
-	//
-	const methods = useForm<IFormInput>();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		reset,
+	} = useForm({
+		mode: 'onChange',
+		resolver: yupResolver(schema),
+	});
 	// const { handleSubmit, register } = useForm<IFormInput>();
-	const onSubmit = (data: IFormInput) => console.log(data);
-	// const onSubmit = (data) => console.log(data);
-	//
+	// const onSubmit = (data: IFormInput) => console.log(data);
+	const onSubmit = (data) => {
+		console.log(data);
+		reset();
+	};
+	const [isActive, setIsActive] = useState(false);
+
 	return (
 		<div className={styles['form-box']}>
-			<FormProvider {...methods}>
-				<form
-					className={styles['form']}
-					onSubmit={methods.handleSubmit(onSubmit)}
-					// onSubmit={handleSubmit(onSubmit)}
+			<form className={styles['form']} onSubmit={handleSubmit(onSubmit)}>
+				<span className={styles['input-box']}>
+					<input
+						{...register('clientName')}
+						type="text"
+						className={styles['contacts-form-input']}
+						placeholder="Имя"
+						onFocus={() => setIsActive(true)}
+						onBlur={() => setIsActive(false)}
+					/>
+					<ClearInputButton styleKey={'input-clear-btn'} isHidden={!isActive} />
+				</span>
+
+				<span className={styles['input-box']}>
+					<input
+						{...register('clientTel')}
+						type="tel"
+						className={styles['contacts-form-input']}
+						placeholder="Телефон"
+						onFocus={() => setIsActive(true)}
+						onBlur={() => setIsActive(false)}
+					/>
+					<ClearInputButton styleKey={'input-clear-btn'} isHidden={!isActive} />
+					<span>{errors.clientTel?.message}</span>
+				</span>
+
+				<span className={styles['input-box']}>
+					<input
+						{...register('clientEmail')}
+						type="email"
+						className={styles['contacts-form-input']}
+						placeholder="Email"
+						onFocus={() => setIsActive(true)}
+						onBlur={() => setIsActive(false)}
+					/>
+					<ClearInputButton styleKey={'input-clear-btn'} isHidden={!isActive} />
+					<span>{errors.clientEmail?.message}</span>
+				</span>
+
+				<span className={styles['textarea-box']}>
+					<textarea
+						{...register('clientMessage')}
+						className={styles['contacts-form-input']}
+						placeholder="Сообщение"
+						onFocus={() => setIsActive(true)}
+						onBlur={() => setIsActive(false)}
+					/>
+					<ClearInputButton styleKey={'input-clear-btn'} isHidden={!isActive} />
+				</span>
+
+				<input
+					{...register('clientFile')}
+					className={styles['contacts-form-input']}
+					type="file"
+					id="file"
+					accept=".doc"
+				/>
+				<label
+					className={styles['contacts-form-file-input-label']}
+					htmlFor="file"
 				>
-					{/*  */}
-					{/* <input {...register('name')} /> */}
-					{/*  */}
-					{inputs.map((el) => (
-						<Input key={`key${el.id}`} {...el} />
-					))}
-					<span className={styles['form__notice']}>
-						*В формате Документ Word
-					</span>
-					<button className={styles['form__button']} type="submit">
-						Отправить
-					</button>
-					<span className={styles['form__notice']}>
-						Нажимая кнопку «отправить», вы соглашаетесь с политикой обработки
-						персональных данных.
-					</span>
-				</form>
-			</FormProvider>
+					Прикрепить ТЗ
+				</label>
+
+				<span className={styles['form__notice']}>*В формате Документ Word</span>
+				<button className={styles['form__button']} type="submit">
+					Отправить
+				</button>
+				<span className={styles['form__notice']}>
+					Нажимая кнопку «отправить», вы соглашаетесь с политикой обработки
+					персональных данных.
+				</span>
+			</form>
 		</div>
 	);
 }
