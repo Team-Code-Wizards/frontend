fd3ba6371421fcf87a6e1d1ecb4612a8f084478e
ignateva-darya@mail.ru <dari.komarenko@gmail.com>
2024-02-09 13:37:19 +0300

    style(faq-plus-style): добавила поворот плюсика при клике

--- src/components/Faq/FaqCard/index.tsx
+++ src/components/Faq/FaqCard/index.tsx
@@ -1,31 +1,37 @@
-import PlusIcon from "../../../../public/images/icons/Plus";
-import { IFaqCardProps } from "./interface";
-import styles from "./styles.module.scss";
+import PlusIcon from '../../../../public/images/icons/Plus';
+import { IFaqCardProps } from './interface';
+import styles from './styles.module.scss';
 
 function FaqCard({
-  item,
-  handleClick,
+	item,
+	handleClick,
 }: {
-  item: IFaqCardProps;
-  handleClick: (question: IFaqCardProps) => void;
+	item: IFaqCardProps;
+	handleClick: (question: IFaqCardProps) => void;
 }) {
-  return (
-    <li className={styles["faq-card"]}>
-      <details
-        open={item.active}
-        onClick={(e) => {
-          e.preventDefault();
-          handleClick(item);
-        }}
-      >
-        <summary className={styles["faq-card__header"]} onClick={() => {}}>
-          <h4 className={styles["faq-card__title"]}>{item.title}</h4>
-          <PlusIcon />
-        </summary>
-        <p className={styles["faq-card__description"]}>{item.description}</p>
-      </details>
-    </li>
-  );
+	return (
+		<li className={styles['faq-card']}>
+			<details
+				open={item.active}
+				onClick={(e) => {
+					e.preventDefault();
+					handleClick(item);
+				}}
+			>
+				<summary className={styles['faq-card__header']} onClick={() => {}}>
+					<h4 className={styles['faq-card__title']}>{item.title}</h4>
+					<p
+						className={`${styles['faq-card__icon']} ${
+							item.active ? styles['faq-card__icon-expanded'] : ''
+						}`}
+					>
+						<PlusIcon />
+					</p>
+				</summary>
+				<p className={styles['faq-card__description']}>{item.description}</p>
+			</details>
+		</li>
+	);
 }
 
 export default FaqCard;
