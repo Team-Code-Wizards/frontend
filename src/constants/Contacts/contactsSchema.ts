1c9e55d3129244c958857a3a0cd7fcc4053b53f6
Ilvina <ilvrakh@gmail.com>
2024-02-25 22:38:38 +0500

    feat: добавила проверку пробелов в имени и длины имейл

--- src/constants/Contacts/contactsSchema.ts
+++ src/constants/Contacts/contactsSchema.ts
@@ -4,7 +4,7 @@ export const contactsSchema = yup.object().shape({
 	name: yup
 		.string()
 		.required('Введите имя')
-		.matches(/^[а-яА-Яa-zA-Z][а-яА-Яa-zA-Z\s]{2,30}$/, 'Неверный формат'),
+		.matches(/^[а-яА-Яa-zA-Z][а-яА-Яa-zA-Z\s]{1,50}$/, 'Неверный формат'),
 	tel: yup
 		.string()
 		.required('Введите номер телефона')
@@ -13,7 +13,7 @@ export const contactsSchema = yup.object().shape({
 		.string()
 		.required('Введите E-mail')
 		.matches(
-			/^([a-zA-Z0-9][a-zA-Z0-9_.-]{1,62}[a-zA-Z0-9])@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z0-9][a-zA-Z\-0-9]{1,64}[a-zA-Z0-9]\.){1,3}[a-zA-Z]{2,64}))$/,
+			/^([a-zA-Z0-9][a-zA-Z0-9_.-]{1,62}[a-zA-Z0-9])@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z0-9][a-zA-Z\-0-9]{1,56}[a-zA-Z0-9]\.){1,3}[a-zA-Z]{2,64}))$/,
 			'Неверный формат E-mail'
 		),
 	message: yup.string().notRequired(),
