cf7c73636226e8eda31f32a2267f6435f0ab16ee
Oleg Smitkevich <o.smitkevich@gmail.com>
2024-02-26 17:31:49 +0300

    fix: фикс логики

--- src/app/api/send-mail/route.ts
+++ src/app/api/send-mail/route.ts
@@ -1,30 +1,20 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
 import sendMail from '@/service/mailservice';
-import { Ratelimit } from '@upstash/ratelimit';
-import { kv } from '@vercel/kv';
-
-const ratelimit = new Ratelimit({
-	redis: kv,
-	// 5 requests from the same IP in 20 seconds
-	limiter: Ratelimit.slidingWindow(5, '20 s'),
-});
-
-// Middleware function to handle rate limiting
-export default async function middleware(request: NextRequest) {
-	const ip = request.ip ?? '127.0.0.1';
-	const { success } = await ratelimit.limit(ip);
-
-	if (success) {
-		return NextResponse.next(request);
-	} else {
-		return NextResponse.redirect(new URL('/blocked', request.url));
-	}
-}
+import middleware from '@/service/middlewareRateLimit';
 
 // Handler for POST requests
-export async function POST(req: Request) {
+export async function POST(req) {
 	try {
+		// Вызов middleware перед обработкой POST-запроса
+		const response = await middleware(req);
+		// Проверка, прошло ли middleware успешно
+		if (response.status !== 200) {
+			// Если middleware вернуло ошибку, возвращаем эту ошибку клиенту
+			return response;
+		}
+
+		// Если middleware прошло успешно, обрабатываем POST-запрос
 		const body = await req.json();
 		sendMail(body);
 		return NextResponse.json({ status: 200 });
