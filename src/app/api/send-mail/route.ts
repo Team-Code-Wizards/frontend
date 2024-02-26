1a71366ac8606f72c69f01f9e41b827f9f4a69db
Oleg Smitkevich <o.smitkevich@gmail.com>
2024-02-26 17:15:05 +0300

    feat: добавляю rate-limit

--- src/app/api/send-mail/route.ts
+++ src/app/api/send-mail/route.ts
@@ -1,15 +1,40 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 import sendMail from '@/service/mailservice';
+import { Ratelimit } from '@upstash/ratelimit';
+import { kv } from '@vercel/kv';
 
+const ratelimit = new Ratelimit({
+	redis: kv,
+	// 5 requests from the same IP in 20 seconds
+	limiter: Ratelimit.slidingWindow(5, '20 s'),
+});
+
+// Define which routes you want to rate limit
+export const config = {
+	matcher: '/',
+};
+
+// Middleware function to handle rate limiting
+export default async function middleware(request: NextRequest) {
+	const ip = request.ip ?? '127.0.0.1';
+	const { success } = await ratelimit.limit(ip);
+
+	if (success) {
+		return NextResponse.next(request);
+	} else {
+		return NextResponse.redirect(new URL('/blocked', request.url));
+	}
+}
+
+// Handler for POST requests
 export async function POST(req: Request) {
 	try {
 		const body = await req.json();
-
 		sendMail(body);
-
 		return NextResponse.json({ status: 200 });
-	} catch {
-		return NextResponse.json({ status: 405 });
+	} catch (error) {
+		console.error(error);
+		return NextResponse.json({ status: 500 });
 	}
 }
