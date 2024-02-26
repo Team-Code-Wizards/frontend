a37d139e26e43d71aa951976c0024be69f13283b
Oleg Smitkevich <o.smitkevich@gmail.com>
2024-02-26 18:59:31 +0300

    feat: добавляю GoogleAnalytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 
 // import { Inter } from 'next/font/google';
 import YandexMetrica from '@/components/YandexMetrica/YandexMetrica';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 import './globals.scss';
 
@@ -21,14 +22,8 @@ export default function RootLayout({
 	return (
 		<html lang="ru" style={{ scrollBehavior: 'smooth' }}>
 			<link rel="icon" href="/favicon.ico" sizes="any" />
-			<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NMKTG44L');</script>
 			<body className="body">
-				<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NMKTG44L"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+				<GoogleAnalytics gaId="GTM-NMKTG44L" />
 				<YandexMetrica />
 				{children}
 			</body>
