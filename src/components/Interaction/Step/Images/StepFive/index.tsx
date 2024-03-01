81ee0c71f10ae8ca1c15419bfc2ea79f1dffbbac
ignateva-darya@mail.ru <dari.komarenko@gmail.com>
2024-03-01 13:28:57 +0300

    feat: добавляет верные стили для анимации

--- src/components/Interaction/Step/Images/StepFive/index.tsx
+++ src/components/Interaction/Step/Images/StepFive/index.tsx
@@ -12,36 +12,40 @@ import styles from './style.module.scss';
 export default function StepFive(inView: boolean) {
 	return (
 		<div className={`${styles['images']} ${inView && styles['images_inview']}`}>
-			<div className={styles['images__lineone']}>
+			<div
+				className={`${styles['images__lineone']} ${inView && styles['images__lineone_inview']}`}
+			>
 				<Image
 					src={imageOne}
 					width={71.55}
 					height={71.55}
 					alt="1"
-					className={styles['images__lineone_one']}
+					className={`${styles['images__lineone']} ${styles['images__lineone_one']} ${inView && styles['images__lineone_inview-one']}`}
 				/>
 				<Image
 					src={imageTwo}
 					width={71.55}
 					height={71.55}
 					alt="2"
-					className={styles['images__lineone_two']}
+					className={`${styles['images__lineone']} ${styles['images__lineone_two']} ${inView && styles['images__lineone_inview-two']}`}
 				/>
 			</div>
-			<div className={styles['images__linetwo']}>
+			<div
+				className={`${styles['images__linetwo']} ${inView && styles['images__linetwo_inview']}`}
+			>
 				<Image
 					src={imageThree}
 					width={71.55}
 					height={71.55}
 					alt="3"
-					className={styles['images__linetwo_one']}
+					className={`${styles['images__linetwo']} ${styles['images__linetwo_one']} ${inView && styles['images__linetwo_inview-one']}`}
 				/>
 				<Image
 					src={imageFour}
 					width={71.55}
 					height={71.55}
 					alt="4"
-					className={styles['images__linetwo_two']}
+					className={`${styles['images__linetwo']} ${styles['images__linetwo_two']} ${inView && styles['images__linetwo_inview-two']}`}
 				/>
 			</div>
 		</div>
