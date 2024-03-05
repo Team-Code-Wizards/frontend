90683d44e2fbfe6b10e3000b76c1d55e6d139585
OleG Smitkevich <114614095+OGSmit@users.noreply.github.com>
2024-03-05 19:17:44 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	const isProduction = process.env.PROD === 'true';
+	console.log('test')
 	console.log(isProduction)
 	console.log(typeof isProduction)
 
