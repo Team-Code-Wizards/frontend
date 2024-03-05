02822f1eb150f96b47b735a8a72d5dc39783135b
Oleg Smitkevich <o.smitkevich@gmail.com>
2024-03-05 17:34:46 +0300

    feat: добавил консоль лог

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	const isProdaction = process.env.NODE_ENV === 'production';
-
+	console.log(process.env.NODE_ENV);
 	return (
 		<html lang="ru" style={{ scrollBehavior: 'smooth' }}>
 			<link rel="icon" href="/favicon.ico" sizes="any" />
