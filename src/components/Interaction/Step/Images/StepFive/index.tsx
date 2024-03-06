333dfb595208489077ca86fc38f86c4a40180d90
Oleg Smitkevich <o.smitkevich@gmail.com>
2024-03-06 18:23:16 +0300

    feat: синхронизация+

--- src/components/Interaction/Step/Images/StepFive/index.tsx
+++ src/components/Interaction/Step/Images/StepFive/index.tsx
@@ -13,39 +13,39 @@ export default function StepFive(inView: boolean, isActive: boolean) {
 			className={`${styles['images']} ${inView && isActive && styles['images_inview']}`}
 		>
 			<div
-				className={`${styles['images__lineone']} ${inView && styles['images__lineone_inview']}`}
+				className={`${styles['images__lineone']} ${inView && isActive && styles['images__lineone_inview']}`}
 			>
 				<Image
 					src={imageOne}
 					width={71.55}
 					height={71.55}
 					alt="1"
-					className={`${styles['images__lineone']} ${styles['images__lineone_one']} ${inView && styles['images__lineone_inview-one']}`}
+					className={`${styles['images__lineone']} ${styles['images__lineone_one']} ${inView && isActive && styles['images__lineone_inview-one']}`}
 				/>
 				<Image
 					src={imageTwo}
 					width={71.55}
 					height={71.55}
 					alt="2"
-					className={`${styles['images__lineone']} ${styles['images__lineone_two']} ${inView && styles['images__lineone_inview-two']}`}
+					className={`${styles['images__lineone']} ${styles['images__lineone_two']} ${inView && isActive && styles['images__lineone_inview-two']}`}
 				/>
 			</div>
 			<div
-				className={`${styles['images__linetwo']} ${inView && styles['images__linetwo_inview']}`}
+				className={`${styles['images__linetwo']} ${inView && isActive && styles['images__linetwo_inview']}`}
 			>
 				<Image
 					src={imageThree}
 					width={71.55}
 					height={71.55}
 					alt="3"
-					className={`${styles['images__linetwo']} ${styles['images__linetwo_one']} ${inView && styles['images__linetwo_inview-one']}`}
+					className={`${styles['images__linetwo']} ${styles['images__linetwo_one']} ${inView && isActive && styles['images__linetwo_inview-one']}`}
 				/>
 				<Image
 					src={imageFour}
 					width={71.55}
 					height={71.55}
 					alt="4"
-					className={`${styles['images__linetwo']} ${styles['images__linetwo_two']} ${inView && styles['images__linetwo_inview-two']}`}
+					className={`${styles['images__linetwo']} ${styles['images__linetwo_two']} ${inView && isActive && styles['images__linetwo_inview-two']}`}
 				/>
 			</div>
 		</div>
