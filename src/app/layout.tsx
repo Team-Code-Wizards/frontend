c857c2880ac38df99a118a7507dcef6d0428e534
JackKerouac1613 <133998734+JackKerouac1613@users.noreply.github.com>
2024-03-06 20:10:13 +0300

    feat: добавил свойство для скролла и поменял отступы у блока 'почему нас выбирают'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import YandexMetrica from '@/components/YandexMetrica/YandexMetrica';
 import { GoogleAnalytics } from '@next/third-parties/google';
 import 'dotenv/config';
+
 import './globals.scss';
 
 export const metadata: Metadata = {
@@ -17,21 +18,28 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	const isProductionXYZtableDown = process.env.PROD === 'true';
-	console.log('test')
-	console.log(isProductionXYZtableDown)
-	console.log(typeof isProductionXYZtableDown)
-	const reverseisProductionXYZtableDown = !isProductionXYZtableDown
+	console.log('test');
+	console.log(isProductionXYZtableDown);
+	console.log(typeof isProductionXYZtableDown);
+	const reverseisProductionXYZtableDown = !isProductionXYZtableDown;
 	return (
-		<html lang="ru" style={{ scrollBehavior: 'smooth' }}>
+		<html lang="ru" style={{ scrollBehavior: 'smooth' }} className="html">
 			<link rel="icon" href="/favicon.ico" sizes="any" />
 			<body className="body">
-				{<p style={{ fontSize: '50px', color: 'white' }}>{isProductionXYZtableDown}</p>}
+				{
+					<p style={{ fontSize: '50px', color: 'white' }}>
+						{isProductionXYZtableDown}
+					</p>
+				}
 				{process.env.PROD === 'debug' && <YandexMetrica />}
 				{process.env.PROD && <YandexMetrica />}
-				{reverseisProductionXYZtableDown ? <GoogleAnalytics gaId="GTM-NMKTG44L" /> : <p style={{ fontSize: '50px' }}>SQL</p>}
+				{reverseisProductionXYZtableDown ? (
+					<GoogleAnalytics gaId="GTM-NMKTG44L" />
+				) : (
+					<p style={{ fontSize: '50px' }}>SQL</p>
+				)}
 				{children}
 			</body>
 		</html>
 	);
 }
-
