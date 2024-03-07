d7a9683491c4563abf6002d31f626a4f41d0903f
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2024-03-07 20:03:19 +0300

    feat: changes acc. to new design

--- public/images/icons/UpButton.tsx
+++ public/images/icons/UpButton.tsx
@@ -1,14 +1,14 @@
 export default function UpButtonIcon() {
 	return (
 		<svg
-			width="16"
-			height="16"
-			viewBox="0 0 16 16"
+			width="17"
+			height="17"
+			viewBox="0 0 17 17"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
 		>
 			<path
-				d="M8 15L8 0.999999M8 0.999999L2 7M8 0.999999L14 7"
+				d="M8.5 13.459L8.5 3.54232M8.5 3.54232L4.25 7.79232M8.5 3.54232L12.75 7.79232"
 				stroke="white"
 				strokeWidth="2"
 				strokeLinecap="square"
