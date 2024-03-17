9849430617117f2de637a663cd1d7a8a6a2e1438
Oleg Smitkevich <o.smitkevich@gmail.com>
2024-03-17 13:52:03 +0300

    feat: добавляю Хедер в nex.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { output: 'standalone' };
 
+module.exports = {
+	async headers() {
+		return [
+			{
+				source: '/*',
+				headers: [
+					{
+						key: 'Access-Control-Allow-Origin',
+						value: 'beta.code-wizards.team',
+					},
+				],
+			},
+		];
+	},
+};
 // коммент
 module.exports = nextConfig;
