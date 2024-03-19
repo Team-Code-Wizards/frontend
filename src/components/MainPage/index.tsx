65b1e0f34eb35a1e62ff82dd5e07a465a48456c4
Oleg Smitkevich <o.smitkevich@gmail.com>
2024-03-19 18:35:31 +0300

    feat: убираю динамические импорты из MainPage и меняю версию package

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,65 +1,63 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-
-import dynamic from 'next/dynamic';
-
 import AdvertisingPromo from '@/components/AdvertisingPromo';
 import { InfoMsgProvider } from '@/components/InfoMsgContext';
 
-import ComponentsContainer from '../ComponentsContainer';
 // import MainPromo from '../MainPromo/MainPromo';
+import AboutUs from '../AboutUs';
+import ComponentsContainer from '../ComponentsContainer';
+import Contacts from '../Contacts';
+import Faq from '../Faq';
+import Footer from '../Footer';
+import Guarantee from '../Guarantee/index';
+import Interaction from '../Interaction/index';
 import Navbar from '../Navbar';
 import Popup from '../Popup';
 import Porfolio from '../Portfolio';
+import Recommendations from '../Recommendations';
+import Services from '../Services';
+import StageCreate from '../StageCreate';
+import TechStack from '../TechStack';
+import Choice from '../Сhoice';
 import styles from './style.module.scss';
 
-const StageCreateLazy = dynamic(() => import('../StageCreate'));
-const RecommendationsLazy = dynamic(() => import('../Recommendations'));
-const ChoiceLazy = dynamic(() => import('../Сhoice'));
-const GuaranteeLazy = dynamic(() => import('../Guarantee/index'));
-const InteractionLazy = dynamic(() => import('../Interaction/index'));
-const TechStackLazy = dynamic(() => import('../TechStack'));
-const FaqLazy = dynamic(() => import('../Faq'));
-const ContactsLazy = dynamic(() => import('../Contacts'));
-const FooterLazy = dynamic(() => import('../Footer'));
-const AboutUsLazy = dynamic(() => import('../AboutUs'));
-const ServicesLazy = dynamic(() => import('../Services'));
+// const StageCreateLazy = dynamic(() => import('../StageCreate'));
+// const RecommendationsLazy = dynamic(() => import('../Recommendations'));
+// const ChoiceLazy = dynamic(() => import('../Сhoice'));
+// const GuaranteeLazy = dynamic(() => import('../Guarantee/index'));
+// const InteractionLazy = dynamic(() => import('../Interaction/index'));
+// const TechStackLazy = dynamic(() => import('../TechStack'));
+// const FaqLazy = dynamic(() => import('../Faq'));
+// const ContactsLazy = dynamic(() => import('../Contacts'));
+// const FooterLazy = dynamic(() => import('../Footer'));
+// const AboutUsLazy = dynamic(() => import('../AboutUs'));
+// const ServicesLazy = dynamic(() => import('../Services'));
 
 export default function MainPage() {
-	const [isLoadMore, setIsLoadMore] = useState(false);
-	useEffect(() => {
-		setIsLoadMore(true);
-	}, []);
-
 	return (
-		<InfoMsgProvider>
-			<main className={styles.main}>
+		<main className={styles.main}>
+			<InfoMsgProvider>
 				<AdvertisingPromo />
 				{/* <MainPromo /> */}
 				<Navbar />
 				<ComponentsContainer>
-					<AboutUsLazy />
-					<ServicesLazy />
+					<AboutUs />
+					<Services />
 					<Porfolio />
 				</ComponentsContainer>
-				{isLoadMore && (
-					<>
-						<StageCreateLazy />
-						<RecommendationsLazy />
-						<ChoiceLazy />
-						<GuaranteeLazy />
-						<InteractionLazy />
-						<TechStackLazy />
-						<ComponentsContainer>
-							<FaqLazy />
-							<ContactsLazy />
-							<FooterLazy />
-						</ComponentsContainer>
-						<Popup />
-					</>
-				)}
-			</main>
-		</InfoMsgProvider>
+				<StageCreate />
+				<Recommendations />
+				<Choice />
+				<Guarantee />
+				<Interaction />
+				<TechStack />
+				<ComponentsContainer>
+					<Faq />
+					<Contacts />
+					<Footer />
+				</ComponentsContainer>
+				<Popup />
+			</InfoMsgProvider>
+		</main>
 	);
 }
