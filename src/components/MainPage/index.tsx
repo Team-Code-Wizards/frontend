e44a7c1e7d97bc9427daad78e2ffa1e2ea92cf5f
Oleg Smitkevich <o.smitkevich@gmail.com>
2024-03-21 16:46:02 +0300

    feat: убираю динамические импорты

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -5,52 +5,49 @@
 // import StageCreate from '../StageCreate';
 // import TechStack from '../TechStack';
 // import Choice from '../Сhoice';
-import { useEffect, useState } from 'react';
-
-import dynamic from 'next/dynamic';
-
+// import { useEffect, useState } from 'react';
+// import dynamic from 'next/dynamic';
 import AdvertisingPromo from '@/components/AdvertisingPromo';
 import { InfoMsgProvider } from '@/components/InfoMsgContext';
 
 // import MainPromo from '../MainPromo/MainPromo';
 import AboutUs from '../AboutUs';
 import ComponentsContainer from '../ComponentsContainer';
-// import Contacts from '../Contacts';
-// import Faq from '../Faq';
-// import Footer from '../Footer';
-// import Guarantee from '../Guarantee/index';
-// import Interaction from '../Interaction/index';
+import Contacts from '../Contacts';
+import Faq from '../Faq';
+import Footer from '../Footer';
+import Guarantee from '../Guarantee/index';
+import Interaction from '../Interaction/index';
 import Navbar from '../Navbar';
-// import Popup from '../Popup';
+import Popup from '../Popup';
 import Porfolio from '../Portfolio';
+import Recommendations from '../Recommendations';
+import Services from '../Services';
+import StageCreate from '../StageCreate';
+import TechStack from '../TechStack';
+import Choice from '../Сhoice';
 import styles from './style.module.scss';
 
 // const ComponentsContainerLazy = dynamic(() => import('../ComponentsContainer'));
 // const PorfolioLazy = dynamic(() => import('../Portfolio'), { ssr: false, });
-const PopupLazy = dynamic(() => import('../Popup'), { ssr: false });
-const StageCreateLazy = dynamic(() => import('../StageCreate'), { ssr: false });
-const RecommendationsLazy = dynamic(() => import('../Recommendations'), {
-	ssr: false,
-});
-const ChoiceLazy = dynamic(() => import('../Сhoice'), { ssr: false });
-const GuaranteeLazy = dynamic(() => import('../Guarantee/index'), {
-	ssr: false,
-});
-const InteractionLazy = dynamic(() => import('../Interaction/index'), {
-	ssr: false,
-});
-const TechStackLazy = dynamic(() => import('../TechStack'), { ssr: false });
-const FaqLazy = dynamic(() => import('../Faq'), { ssr: false });
-const ContactsLazy = dynamic(() => import('../Contacts'), { ssr: false });
-const FooterLazy = dynamic(() => import('../Footer'), { ssr: false });
+// const PopupLazy = dynamic(() => import('../Popup'), { ssr: false });
+// const StageCreateLazy = dynamic(() => import('../StageCreate'), { ssr: false });
+// const RecommendationsLazy = dynamic(() => import('../Recommendations'), {ssr: false,});
+// const ChoiceLazy = dynamic(() => import('../Сhoice'), { ssr: false });
+// const GuaranteeLazy = dynamic(() => import('../Guarantee/index'), {ssr: false,});
+// const InteractionLazy = dynamic(() => import('../Interaction/index'), {ssr: false,});
+// const TechStackLazy = dynamic(() => import('../TechStack'), { ssr: false });
+// const FaqLazy = dynamic(() => import('../Faq'), { ssr: false });
+// const ContactsLazy = dynamic(() => import('../Contacts'), { ssr: false });
+// const FooterLazy = dynamic(() => import('../Footer'), { ssr: false });
 // const AboutUsLazy = dynamic(() => import('../AboutUs'), { ssr: false, });
-const ServicesLazy = dynamic(() => import('../Services'), { ssr: false });
+// const ServicesLazy = dynamic(() => import('../Services'), { ssr: false });
 
 export default function MainPage() {
-	const [isLoadMore, setIsLoadMore] = useState(false);
-	useEffect(() => {
-		setIsLoadMore(true);
-	}, []);
+	// const [isLoadMore, setIsLoadMore] = useState(false);
+	// useEffect(() => {
+	// 	setIsLoadMore(true);
+	// }, []);
 
 	return (
 		<main className={styles.main}>
@@ -59,25 +56,21 @@ export default function MainPage() {
 				<Navbar />
 				<ComponentsContainer>
 					<AboutUs />
-					{isLoadMore && <ServicesLazy />}
+					<Services />
 					<Porfolio />
 				</ComponentsContainer>
-				{isLoadMore && (
-					<>
-						<StageCreateLazy />
-						<RecommendationsLazy />
-						<ChoiceLazy />
-						<GuaranteeLazy />
-						<InteractionLazy />
-						<TechStackLazy />
-						<ComponentsContainer>
-							<FaqLazy />
-							<ContactsLazy />
-							<FooterLazy />
-						</ComponentsContainer>
-						<PopupLazy />
-					</>
-				)}
+				<StageCreate />
+				<Recommendations />
+				<Choice />
+				<Guarantee />
+				<Interaction />
+				<TechStack />
+				<ComponentsContainer>
+					<Faq />
+					<Contacts />
+					<Footer />
+				</ComponentsContainer>
+				<Popup />
 			</InfoMsgProvider>
 		</main>
 	);
