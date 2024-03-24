383aec0f0ee7dab3af0c72820a1081bc6e9b0419
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2024-03-24 16:27:26 +0300

    feat: гарантии слайдер и заголовок

--- src/components/Guarantee/index.tsx
+++ src/components/Guarantee/index.tsx
@@ -43,11 +43,14 @@ function Guarantee() {
 					modules={[Pagination]}
 					className={styles['guarantee__list']}
 				>
-					{dataCard.map((item) =>
+					{dataCard.map((item, index) =>
 						width < 701 ? (
 							<SwiperSlide
 								className={styles['guarantee__card']}
-								style={{ width: 'fit-content' }}
+								style={{
+									width: 'fit-content',
+									marginLeft: index === 6 ? '0' : '20px',
+								}}
 								key={item.title}
 							>
 								<GuaranteeCard key={item.id} item={item} />
